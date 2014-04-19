01cf91b54998334e5df0874614f640fa5e7b8674
Sonali <sonali.sharma@ischool.berkeley.edu>
2014-04-19 13:03:22 -0700

    First Code

---
+++ src/test/CardsTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by sonali on 4/19/14.
+ */
+
+public class CardsTest {
+
+    @Test
+    public void  shouldreturnAreaOfTenforTwoByFiveRectangle()
+    {
+        Cards rectangle = new Cards(5,2);
+        assertEquals(10,rectangle.area());
+    }
+
+}
