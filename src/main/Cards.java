c3d42b5068ce76b71978254b7fd94f11d8b1ddb4
Sonali <sonali.sharma@ischool.berkeley.edu>
2014-04-19 13:18:38 -0700

    Adding first class

---
+++ src/main/Cards.java
@@ -0,0 +1,18 @@
+/**
+ * Created by sonali on 4/19/14.
+ */
+
+public class Cards {
+    int width;
+    int height;
+
+    public Cards(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int area() {
+        return 10;
+    }
+}
